23fca83084502fe5052a066073d59f3e1497be6d
erhnam <i32arrof@uco.es>
2016-11-14 11:28:09 +0100

    actualizacion en base de datos

--- raspirover/models.py
+++ raspirover/models.py
@@ -3,9 +3,18 @@ from django.db import models
 from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.models import User 
-from django.core.validators import MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+def validate_only_one_instance(object):
+	"""
+	Valida que solo exista una instancia del modelo
+	"""
+	model = object.__class__
+	if (model.objects.count() > 0 and object.id != model.objects.get().id):
+		raise ValidationError('Sólo una instancia del modelo %s es permitida.' % model.__name__)
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True, related_name='user_profile')
@@ -20,30 +29,82 @@ class Sensor(models.Model):
 	descripcion = models.CharField(max_length=140)
 	fecha = models.DateTimeField(auto_now_add=True, null=True)
 
-
 class SensorTemperatura(Sensor):
-	temperatura = models.FloatField(null=True)
+	temperatura = models.FloatField(null=True,
+		validators=[
+			MinValueValidator(-10),
+			MaxValueValidator(50)
+		])	
+	enable = models.BooleanField(default=False)
 
 	def __unicode__(self):
-		return "Temperatura"
-		
+		return u'%s' % (self.nombre)
+
+	def clean(self):
+		validate_only_one_instance(self)
+
+	def save(self, *args, **kwargs):
+		super(TemperatureSensor, self).save(*args, **kwargs)
+		self.full_clean()
+		if not self.name:
+			self.name = 'SensorTemperatura' + str(self.id)
+			self.save()
+
+
 class SensorHumedad(Sensor):
-	humedad = models.FloatField(null=True)
+	humedad = models.FloatField(null=True,
+		validators=[
+			MinValueValidator(-10),
+			MaxValueValidator(50)
+		])	
+	enable = models.BooleanField(default=False)
 
 	def __unicode__(self):
-		return "Humedad"
-		
+		return u'%s' % (self.nombre)
+	
+	def clean(self):
+		validate_only_one_instance(self)
+
+	def save(self, *args, **kwargs):
+		super(TemperatureSensor, self).save(*args, **kwargs)
+		self.full_clean()
+		if not self.name:
+			self.name = 'SensorHumedad' + str(self.id)
+			self.save()
+				
 class SensorLuz(Sensor):
 	luz = models.IntegerField(null=True)
+	enable = models.BooleanField(default=False)
 
 	def __unicode__(self):
-		return "Luz"
-		
+		return u'%s' % (self.nombre)
+
+	def clean(self):
+		validate_only_one_instance(self)
+
+	def save(self, *args, **kwargs):
+		super(TemperatureSensor, self).save(*args, **kwargs)
+		self.full_clean()
+		if not self.name:
+			self.name = 'SensorLuz' + str(self.id)
+			self.save()
+
 class SensorGas(Sensor):
 	gas = models.IntegerField(null=True)
+	enable = models.BooleanField(default=False)
 
 	def __unicode__(self):
-		return "Gas"
+		return u'%s' % (self.nombre)
+
+	def clean(self):
+		validate_only_one_instance(self)
+
+	def save(self, *args, **kwargs):
+		super(TemperatureSensor, self).save(*args, **kwargs)
+		self.full_clean()
+		if not self.name:
+			self.name = 'SensorGas' + str(self.id)
+			self.save()
 
 class Exploracion(models.Model):
 	id_exploracion = models.AutoField(primary_key=True)
@@ -55,9 +116,7 @@ class Exploracion(models.Model):
 	tiempo = models.DecimalField(max_digits=3, decimal_places=1, validators=[MaxValueValidator(10)], null=True)
 
 	def __unicode__(self):
-		return self.nombre		
-
-
+		return u'%s' % (self.nombre)	
 		
 class Sensores(models.Model):
 	nombre = models.CharField(max_length=12, blank=False)
