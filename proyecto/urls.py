f9593da43d914351fa46e9d585892d0802d8ba27
erhnam <i32arrof@uco.es>
2016-12-05 11:04:19 +0100

    django-debug-toolbar

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 from raspirover import views
+import debug_toolbar
   
 admin.autodiscover()
 
@@ -13,6 +14,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),    
     url (r'^login', views.login, name='login'),
     url (r'^logout', views.logout, name='logout'),
+    url(r'^__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
