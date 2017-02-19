e12927199e19bfe0095f34415e17ad4988482036
i32arrof <i32arrof@uco.es>
2017-02-19 16:36:06 +0100

    coordenadas gps

--- gps.py
+++ gps.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import serial
 import pynmea2
 import globales
@@ -13,8 +15,11 @@ def parseGPS(str):
 	print 'latitud: ', globales.latitud
 	print 'longitud: ', globales.longitud
 	print 'altura: ', globales.altura
+	alt  = "{:2d}".format(int(msg.latitude)) + 'º' + "{:2d}".format(int(msg.latitude_minutes)) + '′' + "{:7.4f}".format(msg.latitude_seconds) + '"'
+        lon  = "{:2d}".format(int(msg.longitude)) + 'º' + "{:2d}".format(int(msg.longitude_minutes)) + '′' + "{:7.4f}".format(msg.longitude_seconds) + '"'
+	
 	data = open("locations.txt", "a")
-	data.write("%s,%s\n" % (msg.lat, msg.lon))
+	data.write("%s,%s\n" % ( alt, lon ))
 	data.close()	
 
 serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
